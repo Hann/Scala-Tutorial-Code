21b6f5bf178bc0dfe7d89e462c69854c3bfe9dd9
Jin-Soo Han <jinsu0411@gmail.com>
2017-03-25 12:19:21 +0900

    Basic Syntax 단원 추가

---
+++ Basic Syntax/HelloWorld.scala
@@ -0,0 +1,10 @@
+object HelloWorld {
+    /*
+     * 주석을 이렇게도 입력할 수 있네!
+     */
+    // 여긴 메인이고.. 먼저 param의 이름을 쓰고 그 담에 타입을 쓴다.
+    def main(args: Array[String]){
+        // 프린트 엘엔 너무 반갑네!
+        println("Hello World!")
+    }
+}
