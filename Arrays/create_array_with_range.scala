79ada233a01c4bcd9bf2fc9b38ce5567cda6a3d6
Jin-Soo Han <jinsu0411@gmail.com>
2017-04-09 19:30:30 +0900

    주석 추가

--- Arrays/create_array_with_range.scala
+++ Arrays/create_array_with_range.scala
@@ -2,8 +2,8 @@ import Array._
 
 object Demo {
     def main(args: Array[String]){
-        var myList1 = range(10, 20, 2)
-        var myList2 = range(10, 20)
+        var myList1 = range(10, 20, 2) //근데 20은 포함이 안된다 2칸씩
+        var myList2 = range(10, 20) // 파이썬의 range와 비슷함
 
         for (x <- myList1){
             print( " " + x)
