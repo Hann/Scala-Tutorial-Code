9d4865b461d6414dc6f5378cb525f56fa3af0d04
Jin-Soo Han <jinsu0411@gmail.com>
2017-05-10 20:21:31 +0900

    Partially Applied Functions 서브 단원 추가

---
+++ Functions/Partially Applied Functions/partially.scala
@@ -0,0 +1,19 @@
+import java.util.Date
+
+object Demo {
+    def main(args: Array[String]) {
+        val date = new Date
+        val logWithDateBound = log(date, _: String)
+        logWithDateBound("message1")
+
+        Thread.sleep(100)
+        logWithDateBound("message2")
+
+        Thread.sleep(100)
+        logWithDateBound("message3")
+    }
+
+    def log(date: Date, message: String){
+        println(date + "-----" + message)
+    }
+}
